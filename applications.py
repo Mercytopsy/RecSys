259bec92251ebe6aaaa33c3c0c46fca4af21835a
BABATOPE <odunayo927@gmail.com>
2020-01-29 22:11:36 +0100

    new_change

--- applications.py
+++ applications.py
@@ -20,6 +20,11 @@ def create_app(**config_overrides):
 
     # apply overrides for tests
     #app.config.update(config_overrides)
+    app.config.from_mapping(
+        SECRET_KEY = os.environ.get('SECRET_KEY'),
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL'),
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+    )
 
     # initialize db
     db.init_app(app)
