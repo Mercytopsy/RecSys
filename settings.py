406d99ff88538d7abf74054ae5170d6ca39c1fab
BABATOPE <odunayo927@gmail.com>
2020-02-02 12:12:23 +0100

    all

--- settings.py
+++ settings.py
@@ -1,11 +1,11 @@
 import os
 
-SECRET_KEY=os.environ.get['SECRET_KEY']
-DB_USERNAME=os.environ.get['DB_USERNAME']
-DB_PASSWORD=os.environ.get['DB_PASSWORD']
-DB_HOST=os.environ.get['DB_HOST']
-DATABASE_NAME=os.environ.get['DATABASE_NAME']
-DB_URI=os.environ.get('CLEARDB_DATABASE_URL')
+SECRET_KEY=os.environ['SECRET_KEY']
+DB_USERNAME=os.environ['DB_USERNAME']
+DB_PASSWORD=os.environ['DB_PASSWORD']
+DB_HOST=os.environ['DB_HOST']
+DATABASE_NAME=os.environ['DATABASE_NAME']
+DB_URI=os.environ['CLEARDB_DATABASE_URL']
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 BLOG_NAME=os.environ['BLOG_NAME']
