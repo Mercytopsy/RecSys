dcd127e06995709e5742730f7eecf7d428aad7fe
BABATOPE <odunayo927@gmail.com>
2020-02-02 12:06:35 +0100

    all

--- applications.py
+++ applications.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flaskext.markdown import Markdown
 from flask_mail import Mail, Message
-from flask_heroku import Heroku
+#from flask_heroku import Heroku
 #from flaskext.mysql import MySQL
 import os
 #from flask.ext.heroku import Heroku
@@ -24,15 +24,11 @@ def create_app(**config_overrides):
 
     # apply overrides for tests
     #app.config.update(config_overrides)
-    
-    app.config.from_mapping(
-        SECRET_KEY = os.environ.get('SECRET_KEY'),
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL'),
-        SQLALCHEMY_TRACK_MODIFICATIONS = False
-    )
+    app.config.from_pyfile('settings.py')
+ 
 
     # initialize db
-    heroku=Heroku(app)
+    #heroku=Heroku(app)
     db.init_app(app)
     migrate = Migrate(app, db)
     # Markdown
